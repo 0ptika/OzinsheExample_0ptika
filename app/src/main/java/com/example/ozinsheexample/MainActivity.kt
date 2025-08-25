ec3d8b03dd62c1a89c28ecb21b0fbe2f07e1fab7
0ptika <oleshchuk05@gmail.com>
2025-08-25 23:39:49 +0600

    1

--- app/src/main/java/com/example/ozinsheexample/MainActivity.kt
+++ app/src/main/java/com/example/ozinsheexample/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
 import com.example.ozinsheexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        findNavController()
 
     }
 }
